4fd41f47fe953ea946101b783056439d7bf27f80
swapnil <swapnil130501@gmail.com>
2025-07-06 00:44:24 +0530

    fix: content for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <Heading>Swapnil Lohani</Heading>
-            <SubHeading> I'm a software engineer with a passion for creating efficient and scalable web applications. Experienced in designing and implementing robust APIs, optimizing database performance, and ensuring seamless integration with front-end systems. Proficient in a wide range of programming languages and frameworks, including Express, Node.js, and Java script.</SubHeading>
+            <SubHeading> I’m a frontend engineer who enjoys turning ideas into interactive, accessible, and pixel-perfect web experiences. I specialize in building responsive user interfaces with React and Tailwind CSS, and I care deeply about performance, usability, and clean design. Whether it’s crafting smooth animations, implementing complex state logic, or collaborating with backend teams — I love building UI that just feels right.</SubHeading>
             <Projects data={projects?.slice(1, 4)} layout="md:grid-cols-3" />
         </>
     );
