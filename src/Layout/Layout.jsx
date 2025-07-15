a0dc7c14510b175faa00dc56b4949e17e16225de
swapnil <swapnil130501@gmail.com>
2025-07-15 20:08:56 +0530

    Fix: layout bg color dark to 800

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import { Toaster } from "sonner";
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-white dark:bg-neutral-900 min-h-screen">
+    <div className="bg-white dark:bg-neutral-800 min-h-screen">
       <Navbar />
       <Container>{children}</Container>
       <Toaster position="top-center" richColors />
